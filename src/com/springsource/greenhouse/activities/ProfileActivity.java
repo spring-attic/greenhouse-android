4a9c24eb408b62e84ad4269200a0d8dbcd0efd8d
Roy Clarkson <rclarkson@Avengers-MacBook-Pro.local>
2010-10-11 18:25:46 -0400

    Working on the profile screen layout

--- src/com/springsource/greenhouse/activities/ProfileActivity.java
+++ src/com/springsource/greenhouse/activities/ProfileActivity.java
@@ -2,15 +2,34 @@ package com.springsource.greenhouse.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.springsource.greenhouse.R;
+import com.springsource.greenhouse.R.drawable;
 
 public class ProfileActivity extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile);
 		
-		TextView textView = new TextView(this);
-		textView.setText("This is the Profile tab");
-		setContentView(textView);
+		final TextView textViewMemberName = (TextView) findViewById(R.id.member_name);
+		final ImageView imageViewPicture = (ImageView) findViewById(R.id.picture);
+		final Button buttonSignOut = (Button) findViewById(R.id.signout);
+		
+		textViewMemberName.setText("John Smith");
+		
+		imageViewPicture.setImageResource(drawable.profile);
+		
+		buttonSignOut.setOnClickListener(new OnClickListener() {
+		    public void onClick(View v) {
+		        Toast.makeText(ProfileActivity.this, "If I was working, I would sign out now.", Toast.LENGTH_SHORT).show();
+		    }
+		});
 	}
 }
