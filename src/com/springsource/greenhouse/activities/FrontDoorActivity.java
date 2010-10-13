e85aa6d3ec7895b816fa85247b469f6da9252a93
Craig Walls <cwalls@vmware.com>
2010-10-13 16:16:25 -0500

    Added foot-in-the-door activity that will ultimately be used to determine whether or not the user has signed in.

---
+++ src/com/springsource/greenhouse/activities/FrontDoorActivity.java
@@ -0,0 +1,15 @@
+package com.springsource.greenhouse.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class FrontDoorActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Intent intent = new Intent(this, MainTabWidget.class);
+		startActivity(intent);
+		finish();
+	}
+}
