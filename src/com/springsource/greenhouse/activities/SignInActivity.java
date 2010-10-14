b8ab86342f6ff815ca8b5fb29be519023a8699f8
Craig Walls <cwalls@vmware.com>
2010-10-14 14:08:01 -0500

    Put in temporary bypass of OAuth stuff so that I can commit and not break Roy's work.

--- src/com/springsource/greenhouse/activities/SignInActivity.java
+++ src/com/springsource/greenhouse/activities/SignInActivity.java
@@ -1,14 +1,12 @@
 package com.springsource.greenhouse.activities;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.springsource.greenhouse.R;
-import com.springsource.greenhouse.util.Prefs;
 
 public class SignInActivity extends Activity {
 
@@ -19,7 +17,9 @@ public class SignInActivity extends Activity {
 
 		findViewById(R.id.buttonSignin).setOnClickListener(new OnClickListener() {
 			public void onClick(final View view) {
-				startActivity(new Intent(SignInActivity.this, OAuthActivity.class));
+				// uncomment the following line (and comment the line after it) to enable the OAuth stuff
+//				startActivity(new Intent(SignInActivity.this, OAuthActivity.class));
+				startActivity(new Intent(SignInActivity.this, MainTabWidget.class));
 				finish();
 			}
 		});
