e0688d5fdcfaeab37b155e1d59a10d60f0315ea6
Craig Walls <cwalls@vmware.com>
2010-10-14 13:34:30 -0500

    Changed logo on signin screen

--- src/com/springsource/greenhouse/activities/FrontDoorActivity.java
+++ src/com/springsource/greenhouse/activities/FrontDoorActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.springsource.greenhouse.util.Prefs;
 
--- src/com/springsource/greenhouse/activities/SignInActivity.java
+++ src/com/springsource/greenhouse/activities/SignInActivity.java
@@ -1,12 +1,14 @@
 package com.springsource.greenhouse.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.springsource.greenhouse.R;
+import com.springsource.greenhouse.util.Prefs;
 
 public class SignInActivity extends Activity {
 
